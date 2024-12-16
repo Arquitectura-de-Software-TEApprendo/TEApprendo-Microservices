f3da671bf7879c0cb6b1dfe551d3f1e4b5b179ac
Jimena Ruiz <jyellow363@gmail.com>
2024-12-16 13:46:38 -0500

    Fix for deploy in AWS.

---
+++ game-service/src/main/java/com/hexagon/gameservice/shared/infrastructure/eureka/EurekaConfig.java
@@ -0,0 +1,21 @@
+package com.hexagon.gameservice.shared.infrastructure.eureka;
+
+import com.netflix.appinfo.AmazonInfo;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class EurekaConfig {
+
+    @Bean
+    @Profile("prod")
+    public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+        EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        b.setDataCenterInfo(info);
+        return b;
+    }
+}
